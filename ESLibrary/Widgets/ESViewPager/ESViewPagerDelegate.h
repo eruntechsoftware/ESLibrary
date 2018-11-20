250c4809d896813167771396cfa4fbb737c05ad6
杜明悦 <dmy229@gmail.com>
2018-11-20 15:14:29 +0800

    no message

---
+++ ESLibrary/Widgets/ESViewPager/ESViewPagerDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ESViewPagerDelegate.h
+//  Neighbour
+//
+//  Created by 杜明悦 on 2016/10/18.
+//  Copyright © 2016年 杜明悦. All rights reserved.
+//  ESViewPager 协议
+
+@protocol ESViewPagerDelegate <NSObject>
+
+/**
+ 滑动结束后执行的处理
+ @param index 索引下标
+ */
+-(void)viewPagerScrollDid:(int)index;
+
+@end
