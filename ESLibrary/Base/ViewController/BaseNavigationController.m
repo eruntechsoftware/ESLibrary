99156d7029ad8fbd29c2d0d7fff77b293bb72ac3
杜明悦 <dmy229@gmail.com>
2018-12-04 10:35:49 +0800

    no message

--- ESLibrary/Base/ViewController/BaseNavigationController.m
+++ ESLibrary/Base/ViewController/BaseNavigationController.m
@@ -168,7 +168,7 @@ static UIColor *NavigationBar_barTintColor;
     {
         UIImageView* backImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 8, 25, 25)];
         backImage.image = [UIImage imageNamed:@"arrow_left_icon"];
-        backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 90, self.navigationBar.frame.size.height)];
+        backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, self.navigationBar.frame.size.height)];
         [backButton addSubview:backImage];
        
         //添加默认处理事件
@@ -179,7 +179,7 @@ static UIColor *NavigationBar_barTintColor;
     }
     else
     {
-        backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 90, self.navigationBar.frame.size.height)];
+        backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, self.navigationBar.frame.size.height)];
         barButton = [[UIBarButtonItem alloc] initWithCustomView:backButton];
     }
     return barButton;
@@ -226,7 +226,7 @@ static UIColor *NavigationBar_barTintColor;
     UIBarButtonItem* barButton;
     if(viewController.navigationItem!=nil){
         //实例化按钮
-        UIButton *backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 40, self.navigationBar.frame.size.height)];
+        UIButton *backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 60, self.navigationBar.frame.size.height)];
         //实例化图片视图
         UIImageView* backImage = [[UIImageView alloc] initWithFrame:CGRectMake(73, self.navigationBar.frame.size.height/2 - 10, 18, 18)];
         backImage.image = img;
